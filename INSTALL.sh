41d01efb08989b8de3a164aceb248b968954c85e
Yifan Yuan <tuji.yyf@alibaba-inc.com>
2022-06-22 12:59:35 +0800

    update INSTALL.sh
      add option about changing cri snapshotter
    
    Signed-off-by: Yifan Yuan <tuji.yyf@alibaba-inc.com>

--- INSTALL.sh
+++ INSTALL.sh
@@ -32,23 +32,43 @@ sudo cp $H/script/overlaybd-snapshotter.service /opt/overlaybd/snapshotter
 sudo systemctl enable /opt/overlaybd/snapshotter/overlaybd-snapshotter.service
 sudo systemctl start overlaybd-snapshotter
 
-getInput 'Would you like make containerd support overlaybd-snapshotter [Y/N]? (this will change /etc/containerd/config.toml and restart containerd)'
+getInput 'Would you like make containerd support overlaybd-snapshotter [Y/N]? It will add proxy_plugin config and restart containerd'
 OP=$?
-if [[ OP -eq 1 ]]; then
+create=0
+installed=0
+if [[ $OP -eq 1 ]]; then
     echo "Change config.toml to make containerd support snapshotter..."
     m=$(grep proxy_plugins.overlaybd /etc/containerd/config.toml)
-    if [[ $? -ne 0 ]]; then
+    installed=$?
+    if [[ $installed -ne 0 ]]; then
         if [[ ! -f /etc/containerd/config.toml ]]; then
             mkdir -p /etc/containerd ## this directory may not exist in ubuntu
             touch /etc/containerd/config.toml
+            create=1
         fi
 sudo cat <<-EOF | sudo tee --append /etc/containerd/config.toml
 [proxy_plugins.overlaybd]
     type = "snapshot"
     address = "/run/overlaybd-snapshotter/overlaybd.sock"
 EOF
-    sudo systemctl restart containerd
+        sudo systemctl restart containerd
     fi
-else
-    echo "install done."
 fi
+
+getInput 'Would you like change the default snapshotter value of cri to overlaybd [Y/N]?'
+OP=$?
+if [[ $OP -eq 1 ]]; then
+    if [[ $create -eq 1 ]]; then
+sudo cat <<-EOF | sudo tee --append /etc/containerd/config.toml
+[plugins.cri]
+    [plugins.cri.containerd]
+        snapshotter = "overlaybd"
+        disable_snapshot_annotations = false
+EOF
+    elif [[ $installed -ne 0 ]]; then
+        sed -i 's/snapshotter = "overlayfs"/snapshotter = "overlaybd"/g' /etc/containerd/config.toml
+        sed -i 's/disable_snapshot_annotations = true/disable_snapshot_annotations = false/g' /etc/containerd/config.toml
+    fi
+    sudo systemctl restart containerd
+fi
+echo "install done."
